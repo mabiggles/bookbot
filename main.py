9bb2056a039f3e117d574bdf4644354e9a6318d0
mabiggles <uofe2003@aol.com>
2024-07-16 11:48:22 -0500

    Count Characters

--- main.py
+++ main.py
@@ -3,7 +3,19 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     print(f"{num_words} words found in the document")
+    print(count_characters(text))
 
+def count_characters(text):
+    text = text.lower()
+
+    dictionary = {}
+    for letter in text:
+        if letter in dictionary:
+            dictionary[letter] += 1
+        else:
+            dictionary[letter] = 1
+
+    return dictionary
 
 def get_num_words(text):
     words = text.split()
